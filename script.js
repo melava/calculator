6934aa0625c810a695b2fe1176ec7b1909331364
Melanie Vanput <melanie.vanput@gmail.com>
2021-01-13 18:18:25 +0100

    reset all script to optimize logic: thinking about steps instead features to combine afterward

--- script.js
+++ script.js
@@ -1,178 +1,146 @@
         /* Definition of variables*/
-let operands;
-let firstOperand;
-let secondOperand;
+// math var
+let firstOperand = '';
+let secondOperand = '';
+let result = '';
 let operation = '';
-let result;
 
-//display
+//html elements
 const displayScreen = document.getElementById('display');
-
-// function buttons
 const clearButton = document.getElementById('clear');
 const deleteButton = document.getElementById('delete');
-
-// operator buttons
-const divideOperator = document.getElementById('divide');
-const multiplyOperator = document.getElementById('multiply');
-const substractOperator = document.getElementById('substract');
-const addOperator = document.getElementById('add');
+const operators = document.querySelectorAll('#operators > div');
 const equal = document.getElementById('equal');
-
-// numbers buttons
 const numbers = document.querySelectorAll('#numbers > div');
-const decimalButtons = document.getElementById('decimal');
 
 
-
-            /* General functions */
-// display numbers and operators
-function display(){
-    if (displayScreen.textContent === '' && this.textContent === '0' || displayScreen.textContent === '' && this.textContent === '.') {
-        // if begin with 0 or decimal, don't display it
-        return
-    } else if (displayScreen.textContent.includes('.') && this.textContent === '.') {
-        // if there is already a decimal, don't display more
-        return
-    } else if (displayScreen.textContent === 'ERROR') {
-        clear();
-        displayScreen.textContent += this.textContent;
-    } else {
-        displayScreen.textContent += this.textContent;
-    }
-}
-
-
+            /* Functions */
 // clear
 function clear() {
     displayScreen.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
     operation = '';
+    result = '';
 };
 
-
-// delete
+//delete
 function del() {
-    if (displayScreen.textContent[displayScreen.textContent.length - 1] === " ") {
-        displayScreen.textContent = displayScreen.textContent.slice(0, -3);
-        operation = '';
-    } else if (displayScreen.textContent === 'ERROR') {
+    if (displayScreen.textContent.includes('ERROR')) {
         clear();
-    } else {
-        displayScreen.textContent = displayScreen.textContent.slice(0, -1);
+    } else if(secondOperand) {
+        secondOperand = secondOperand.slice(0, -1);
+        displayScreen.textContent = firstOperand + ' ' + operation + ' ' + secondOperand;
+    } else if (operation) {
+        operation = '';
+        displayScreen.textContent = firstOperand;
+    } else if (firstOperand) {
+        firstOperand = firstOperand.slice(0, -1);
+        displayScreen.textContent = firstOperand;
     }
 }
 
+//checking input function
+function isPositiveNumber(e) { return e.target.textContent <= 9 && e.target.textContent > 0 ? true : false }
+function isZero(e) { return e.target.textContent == 0 ? true : false }
+function isDecimal(e) { return e.target.textContent === '.' ? true : false }
+function isOperator(e) { return e.target.textContent === '+' || e.target.textContent === '-' || e.target.textContent === '*' || e.target.textContent === '/' ? true : false }
+function isEqual(e) { return e.target.textContent === '=' ? true : false }
 
-        /* Math functions */
-// operate
-function operate() {
-    if(!operation) {return}
-    operands = displayScreen.textContent.split(` ${operation} `)
-    firstOperand = Number(operands[0]);
-    secondOperand = Number(operands[1]);
-    
-    if(operation === '/' && secondOperand == 0) { 
-        displayScreen.textContent = 'ERROR';
-    } else if(!secondOperand) {
-        return
-    } else {
-        if (operation === '+') {
-            result = firstOperand + secondOperand;
-        } else if (operation === '-') {
-            result = firstOperand - secondOperand;
-        } else if (operation === '*') {
-            result = firstOperand * secondOperand;
-        } else if (operation === '/') {
-            result = firstOperand / secondOperand;
+// main display function
+function display(e) {
+    if (displayScreen.textContent.includes('ERROR')) {
+        clear();
+    }
+    if (!firstOperand && !secondOperand && !operation) {                /* when no math variable are set, only numbers are authorized */
+        if (isPositiveNumber(e) || isZero(e)) {
+            firstOperand = this.textContent;
+            displayScreen.textContent = firstOperand;
         }
-        displayScreen.textContent = result;
-        operation = '';
-        firstOperand = '';
-        secondOperand = '';
+    } else if (firstOperand && !secondOperand && !operation) {         /* once firstOperand exist : here are all possible scenario*/
+        if (firstOperand === '0' && (isPositiveNumber(e) || isZero(e))) {       /* if the first character of the first operand is a 0, remove it if following entry is again a number */
+            firstOperand = this.textContent;
+            displayScreen.textContent = firstOperand;
+        } else if (isPositiveNumber(e) || isZero(e)) {                  /* while entries are numbers, adapt the first operand */
+            if(result) {                                                /* if the shown operand is the result of a previous operation, reset the first operand*/
+                firstOperand = '';
+                result = '';
+            }
+            firstOperand += this.textContent;
+            displayScreen.textContent = firstOperand;
+        } else if (isDecimal(e) && !firstOperand.includes('.')) {       /* only accepts 1 decimal per operand */
+            firstOperand += this.textContent;
+            displayScreen.textContent = firstOperand;
+        } else if (isOperator(e)) {                                     /* if next entry is an operator, set the operation var */
+            operation = this.textContent;
+            displayScreen.textContent = firstOperand + ' ' + operation;
+        }
+    } else if (firstOperand && !secondOperand && operation) {           /* one first operand and operator are set, begin to set up the second operand */
+        if (isPositiveNumber(e) || isZero(e)) {
+            secondOperand = this.textContent;
+            displayScreen.textContent = firstOperand + ' ' + operation + ' ' + secondOperand;
+        }
+    } else if (firstOperand && secondOperand && operation) {            /* use same conditions for the second operand as in first operand definition */
+        if (secondOperand === '0' && (isPositiveNumber(e) || isZero(e))) {
+            secondOperand = this.textContent;
+            displayScreen.textContent = firstOperand + ' ' + operation + ' ' + secondOperand;;
+        } else if (isPositiveNumber(e) || isZero(e)) {
+            secondOperand += this.textContent;
+            displayScreen.textContent = firstOperand + ' ' + operation + ' ' + secondOperand;
+        } else if (isDecimal(e) && !secondOperand.includes('.')) {
+            secondOperand += this.textContent;
+            displayScreen.textContent = firstOperand + ' ' + operation + ' ' + secondOperand;
+        } else if (isOperator(e)) {                                     /* if a new operator is entered, let's do the math and reset the var for the next calculus */
+            result = operate(firstOperand, operation, secondOperand);
+            if (result.includes('ERROR')) {
+                operation = '';
+                firstOperand = ''
+                displayScreen.textContent = result;
+            } else {
+                operation = this.textContent;
+                displayScreen.textContent = result + ' ' + operation;
+                firstOperand = result;
+            }
+            secondOperand = '';
+            result = ''
+        } else if (isEqual(e)) {                                        /* only operate calculus if all 3 necessary variables are set */
+            result = operate(firstOperand, operation, secondOperand);
+            displayScreen.textContent = result;
+            firstOperand = result;
+            operation = '';
+            secondOperand = '';
+        }
     } 
 }
 
-// add
-function add() {
-    if (displayScreen.textContent == undefined || displayScreen.textContent === '' || displayScreen.textContent === 'ERROR') {
-        return
-    } else if(displayScreen.textContent.includes('/ 0')) {
-        displayScreen.textContent = 'ERROR';
-    } else if(displayScreen.textContent.includes('+' || '-' || '*' || '/') && displayScreen.textContent[displayScreen.textContent.length - 1] === " ") {
-        return
-    } else if(operation === '') {
-        operation = '+';
-        displayScreen.textContent += ' + ';
+function operate(firstOperand, operation, secondOperand) {
+    firstOperand = Number(firstOperand);
+    secondOperand = Number(secondOperand);
+    if(secondOperand === 0 && operation === '/') {
+        result = 'ERROR: we can\'t divide per 0';
     } else {
-        operate();
-        operation = '+';
-        displayScreen.textContent += ' + ';
+        switch (operation) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                result = firstOperand / secondOperand;
+                break;
+        }
     }
+    return result.toString()
 }
 
-
-// substract
-function substract() {
-    if (displayScreen.textContent == undefined || displayScreen.textContent === '' || displayScreen.textContent === 'ERROR') {
-        return
-    } else if(displayScreen.textContent.includes('/ 0')) {
-        displayScreen.textContent = 'ERROR';
-    } else if(displayScreen.textContent.includes('+' || '-' || '*' || '/') && displayScreen.textContent[displayScreen.textContent.length - 1] === " ") {
-        return
-    } else if (operation === '') {
-        operation = '-';
-        displayScreen.textContent += ' - ';
-    } else {
-        operate();
-        operation = '-';
-        displayScreen.textContent += ' - ';
-    }
-}
-
-// multiply
-function multiply() {
-    if (displayScreen.textContent == undefined || displayScreen.textContent === '' || displayScreen.textContent === 'ERROR') {
-        return
-    } else if(displayScreen.textContent.includes('/ 0')) {
-        displayScreen.textContent = 'ERROR';
-    } else if(displayScreen.textContent.includes('+' || '-' || '*' || '/') && displayScreen.textContent[displayScreen.textContent.length - 1] === " ") {
-        return
-    } else if (operation === '') {
-        operation = '*';
-        displayScreen.textContent += ' * ';
-    } else {
-        operate();
-        operation = '*';
-        displayScreen.textContent += ' * ';
-    }
-}
-
-// divide
-function divide() {
-    if (displayScreen.textContent == undefined || displayScreen.textContent === '' || displayScreen.textContent === 'ERROR') {
-        return
-    } else if(displayScreen.textContent.includes('/ 0')) {
-        displayScreen.textContent = 'ERROR';
-    } else if(displayScreen.textContent.includes('+' || '-' || '*' || '/') && displayScreen.textContent[displayScreen.textContent.length - 1] === " ") {
-        return
-    } else if (operation === '') {
-        operation = '/';
-        displayScreen.textContent += ' / ';
-    } else {
-        operate();
-        operation = '/';
-        displayScreen.textContent += ' / ';
-    }
-}
-
-
-        /* Event listeners */
+        /* Event listeners & function calls */
 
 clearButton.addEventListener('click', clear);
 deleteButton.addEventListener('click', del);
 numbers.forEach(number => {number.addEventListener('click', display)});
-addOperator.addEventListener('click', add);
-substractOperator.addEventListener('click', substract);
-multiplyOperator.addEventListener('click', multiply);
-divideOperator.addEventListener('click', divide);
-equal.addEventListener('click', operate);
+operators.forEach(operator => {operator.addEventListener('click', display)});
+equal.addEventListener('click', display);
